23790e46207e61d570fb96e8490c7329bdd94554
eahonlin-cell <eahonlin@gmail.com>
2026-07-12 23:10:14 +0800

    📱 Service Worker

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = '609-okinawa-v1';
+const ASSETS = [
+  '/609-okinawa/',
+  '/609-okinawa/index.html',
+  '/609-okinawa/manifest.json',
+  '/609-okinawa/icon-192.png',
+  '/609-okinawa/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.map(u => new Request(u, {cache:'reload'}))))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});

7a8e75dbee2623591617df0380207e31e82f81f3
eahonlin-cell <eahonlin@gmail.com>
2026-07-13 00:12:31 +0800

    🔄 SW 強制清除所有快取

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-const CACHE = '609-okinawa-v3';
-const OLD_CACHES = ['609-okinawa-v1', '609-okinawa-v2'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// 清除所有快取並登出
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Network first, cache fallback
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });

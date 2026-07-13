e6b205a9ca73cf55846ad5804bbc845653850ef8
eahonlin-cell <eahonlin@gmail.com>
2026-07-13 08:45:33 +0800

    🗑 SW 清除所有快取

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-// 清除所有快取並登出
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll())
-      .then(clients => clients.forEach(c => c.navigate(c.url)))
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
 });
